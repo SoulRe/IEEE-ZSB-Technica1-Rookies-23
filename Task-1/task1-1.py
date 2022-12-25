589dfe98f6474df0feb5ca4befc76486837473ac
Mohamed Hany <42424592+SoulRe@users.noreply.github.com>
2022-12-25 17:24:38 +0200

    Add files via upload

---
+++ Task-1/task1-1.py
@@ -0,0 +1,12 @@
+#i was checking the code and found i had forgotten about this problem
+#accidentally so i'm uploading it late Soz
+
+def main():
+    length = int(input("length: "))
+    width  = int(input("Width : "))
+    
+    print("Perimeter = ", 2 * (length + width))
+    print("Area      = ", length * width)
+    
+
+main()
