eb887d71b7a756776494dfe17df310cc91d01090
Mohamed Hany <42424592+SoulRe@users.noreply.github.com>
2022-12-26 19:41:17 +0200

    Add files via upload
    this is the correct solution

--- task6/p10.py
+++ task6/p10.py
@@ -1,24 +1,32 @@
-def deletefreind(popularity, friendsnum):
-    for i in range(friendsnum - 1):
-        if popularity[i] < popularity[i + 1]:
-            popularity.pop(i)
-            # print(popularity)
-            return popularity
-    
-
+"""Couldn't solve , got help but i understand how it works"""
 def main():
     testcases = int(input())
+    #doing this case by case (dynamically)
     for i in range(testcases):
+        #self explanatory just getting input as adviced in problem
         input1 = list(map(int, input().split()))
         friendsnum = input1[0]
         iterations = input1[1]
         popularity = list(map(int, input().split()))
-        check = 0
-        for i in range(iterations):
-            popularity = deletefreind(popularity, friendsnum)
-        for i in range(len(popularity)):
-            print(popularity[i], end = " ")
-        print()
-                      
+        
+        #making the new list of inputs
+        newlist = []
+        #looping through the list of inputted popularity
+        for i in popularity:
+            #checking if the num of friends to remove isn't = 0
+            #checking if there is a newlist of friends (newlist)
+            #checking if the last item is bigger than current value in popularity
+            #if so
+            while iterations and newlist and newlist[-1] < i:
+                #remove the last item
+                newlist.pop(-1)
+                #decreasing the iterations by 1
+                iterations -= 1
+            #whatever happens we append the current value of i
+            newlist.append(i)
+            # print(newlist)
+        #just printing
+        for i in newlist:
+            print(i, end = " ")
                       
 main()
